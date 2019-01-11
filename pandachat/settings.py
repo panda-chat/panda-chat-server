3e151c44db2457e91ea373ecfe3933ba852ee9ed
Alex <alexandercstephens@gmail.com>
2019-01-11 09:56:34 -0600

    Add CORS

--- pandachat/settings.py
+++ pandachat/settings.py
@@ -40,17 +40,22 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "channels",
     "storages",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_WHITELIST = ("pandachat.cc",)
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "pandachat.urls"
 
